80fcc87e56ab8bf4fd435e8bb46c70e70d2f95a6
Alan Gomes <alan.silva@partners.srv.br>
2019-08-15 17:55:09 -0300

    initial commit

---
+++ src/main/java/com/gomes/pontointeligente/api/PontoInteligenteApplication.java
@@ -0,0 +1,16 @@
+package com.gomes.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author alangomes
+ * **/
+@SpringBootApplication
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+
+}
